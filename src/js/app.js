37c567157cd3fa55c3c00adbe453bc75c6e72a41
Beksultan <bakytuulubeksultan@gmail.com>
2022-12-02 16:29:36 +0600

    added advantages block

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import * as flsFunctions from './modules/functions.js';
+import advantagesSwiper from './modules/advantagesSwiper.js';
 import i18next from 'i18next';
 
 flsFunctions.isWebp()
+advantagesSwiper()

f4a233f763607fdcf22a73695a2dd4e79ae45be6
HydroOx1d <n-esenov44@mail.ru>
2022-12-06 08:23:07 +0300

    exit menu added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as flsFunctions from './modules/functions.js';
 import advantagesSwiper from './modules/advantagesSwiper.js';
 import i18next from 'i18next';
+import ExitComponent from './modules/exitComponent.js';
 
 flsFunctions.isWebp()
 advantagesSwiper()
@@ -17,4 +18,18 @@ burger.addEventListener('click', () => {
     document.body.style.overflow = 'auto'
     document.querySelector('.header__menu').style.overflow = 'hidden'
   }
+})
+
+
+
+// ExitComponent
+
+const exitComponent = new ExitComponent('.menu_exit');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.exit-menu-btn').forEach(el => {
+    el.addEventListener('click', (e) => {
+      exitComponent.open(e.target.closest('.exit-menu-btn').dataset.markup)
+    })
+  })
 })
---
+++ src/js/modules/exitComponent.js
@@ -0,0 +1,62 @@
+import { getMarkup } from "./exitMarkups.js"
+
+export default class ExitComponent {
+  loader = false;
+  status = true
+  message;
+  markup;
+
+  constructor (selector) {
+    this.$el = document.querySelector(selector)
+  }
+
+  async getMarkups(dataset) {
+    try {
+      const markup = await getMarkup(dataset)
+
+      this.markup = markup
+    } catch(err) {
+      console.log(err)
+      this.status = false
+      this.message = 'Произошла ошибка, попробуйте позже'
+      this.markup = this.message
+    }
+  }
+
+  close() {
+    this.$el.classList.remove('_is-active')
+
+    document.body.style.overflow = 'auto'
+
+    this.destroy()
+  }
+
+  open(dataset) {
+    this.$el.classList.add('_is-active')
+
+    document.body.style.overflow = 'hidden'
+
+    this.getMarkups(dataset).then(() => {
+      this.$content =  this.$el.querySelector('.menu_exit__content')
+      this.render()
+    })
+  }
+
+  render() {
+    this.$content.innerHTML = this.markup;
+    // if (document.querySelector('.menu_exit__button')) {
+    //   document.querySelector('.menu_exit__button').addEventListener('click', () => {
+    //     this.close()
+    //   })
+    // }
+    document.body.addEventListener('click' , (e) => {
+      if(e.target === document.querySelector('.menu_exit__overlay') || !!e.target.closest('.menu_exit__button')) {
+        this.close()
+      }
+    })
+  }
+
+  destroy() {
+    this.$content.innerHTML = ''
+  }
+}
